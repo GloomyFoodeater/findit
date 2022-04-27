e89f715a3866b734e6a89f08387620c7d595abb3
s.paulavets@gmail.com <GloomyFoodeater>
2022-04-27 21:37:16 +0300

    Refactor loadIcons&add maze of 210-4

--- app/src/main/java/com/example/findit/MainActivity.kt
+++ app/src/main/java/com/example/findit/MainActivity.kt
@@ -18,26 +18,27 @@ class MainActivity : AppCompatActivity() {
 
     // Load icons from resources into dictionary
     private fun loadIcons(): Map<String, Bitmap> {
-        // Resource images
-        val chairBit = BitmapFactory.decodeResource(resources, R.drawable.chair)
-        val tableBit = BitmapFactory.decodeResource(resources, R.drawable.table)
-        val armchairLeftBit = BitmapFactory.decodeResource(resources, R.drawable.armchair_left)
-        val armchairRightBit = BitmapFactory.decodeResource(resources, R.drawable.armchair_right)
-        val tableLargeBit = BitmapFactory.decodeResource(resources, R.drawable.table_large)
-        val deskRowBit = BitmapFactory.decodeResource(resources, R.drawable.desk_row)
-        val stallBit = BitmapFactory.decodeResource(resources, R.drawable.stall)
-        val departmentBit = BitmapFactory.decodeResource(resources, R.drawable.department)
 
-        return mapOf(
-            "chair" to chairBit,
-            "table" to tableBit,
-            "armchair_left" to armchairLeftBit,
-            "armchair_right" to armchairRightBit,
-            "table_large" to tableLargeBit,
-            "desk_row" to deskRowBit,
-            "stall" to stallBit,
-            "department" to departmentBit
+        // Map name of icon -> resource id
+        val idMap = mapOf(
+            "chair" to R.drawable.chair,
+            "table" to R.drawable.table,
+            "armchair_top" to R.drawable.armchair_top,
+            "armchair_left" to R.drawable.armchair_left,
+            "armchair_right" to R.drawable.armchair_right,
+            "table_large" to R.drawable.table_large,
+            "table_large_2" to R.drawable.table_large_2,
+            "table_large_2_pc_left" to R.drawable.table_large_2_pc_left,
+            "table_large_2_pc_right" to R.drawable.table_large_2_pc_right,
+            "desk_row" to R.drawable.desk_row,
+            "stall" to R.drawable.stall,
+            "department" to R.drawable.department
         )
+
+        // Map name of icon -> bitmap
+        val imgMap = mutableMapOf<String, Bitmap>()
+        idMap.forEach { imgMap[it.key] = BitmapFactory.decodeResource(resources, it.value) }
+        return imgMap
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,12 +64,12 @@ class MainActivity : AppCompatActivity() {
         val json = assets.open("maze.json").bufferedReader().use {
             it.readText()
         }
-        val maze = Maze(json, 800F, 2000F, icons)
+        val maze = Maze(json, 800F, 1600F, icons)
         val path = maze.findPath(Pair(3, 4), Pair(7, 6))
         Log.i("PATH", path.joinToString(", "))
         val imgView = findViewById<ImageView>(R.id.imageView)
-//        val img = maze.drawMaze()
-        val img = maze.drawPath(path)
+        val img = maze.drawMaze()
+//        val img = maze.drawPath(path)
         imgView.setImageBitmap(img)
     }
 }
