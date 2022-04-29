cb22b420a91c3db46e2a381502862dee6daf9e1f
s.paulavets@gmail.com <GloomyFoodeater>
2022-04-29 16:09:24 +0300

    Deploy path search in NFC & GUI

--- app/src/main/java/com/example/findit/Cell.kt
+++
@@ -1,9 +0,0 @@
-package com.example.findit
-
-data class Cell(
-    val isRoad: Boolean, // Can be passed through irl
-    val isEnd: Boolean, // Can be used as source/destination point on map
-    val iconName: String,
-    val cols: Int, // Number of rows of icon
-    val rows: Int // Number of cols in icon
-)
